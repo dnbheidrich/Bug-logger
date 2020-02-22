a0d30ef20e39d8606d26a70724a6ccc23621af5a
Dieter Heidrich <dnbheidrich@gmail.com>
2020-02-21 17:45:34 -0700

    changes

--- server/models/Bug.js
+++ server/models/Bug.js
@@ -2,11 +2,11 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const Bug = new Schema({
-  closed: { type:  Boolean, required: true },
-  description:  { type: String, required: true },
-  title: { type: String, required: true },
-  reportedBy:  { type: String, required: true },
-  closedDate:  { type: Date, required: true }
+  closed:  Boolean,
+  description: String,
+  title: String,
+  reportedBy:  String,
+  closedDate:  Date,
 }, { timestamps: true, toJSON: { virtuals: true } })
 
 
